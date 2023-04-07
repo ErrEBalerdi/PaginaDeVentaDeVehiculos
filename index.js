c41da91fbad3425825a17e488b36f3313998a6da
Tomas Alejandro Quispe <perrioliarg@gmail.com>
2023-04-07 02:15:40 -0300

    navbar / home / iconos terminados
    hice hasta la seccion de iconos, corregi algunas cosas del css y js, no agregue el parallax quedaba mal

--- index.js
+++ index.js
@@ -3,5 +3,34 @@ let navbar = document.querySelector(".navbar")
 
 menu.onclick = () => {
     menu.classList.toggle("fa-times");
-    menu.classList.toggle("active");
+    navbar.classList.toggle("active");
+}
+
+document.querySelector("#login-btn").onclick = () => {
+    document.querySelector(".login-form-container").classList.toggle("active");
+}
+
+document.querySelector("#close-login-form").onclick = () => {
+    document.querySelector(".login-form-container").classList.toggle("active");
+}
+
+window.onscroll = () => {
+
+    if (window.scrollY > 0) {
+        document.querySelector(".header").classList.add("active");
+    } else {
+        document.querySelector(".header").classList.remove("active");
     }
+
+    menu.classList.remove("fa-times");
+    navbar.classList.remove("active");
+}
+
+window.onload = () => {
+
+    if (window.scrollY > 0) {
+        document.querySelector(".header").classList.add("active");
+    } else {
+        document.querySelector(".header").classList.remove("active");
+    }
+}
