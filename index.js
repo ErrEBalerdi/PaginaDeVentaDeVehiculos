129e168094efd7e832088f7512ab546f180dd0bc
ErrEBalerdi <ricardobalerdi07122000@gmail.com>
2023-04-08 11:36:04 -0300

    featured section html , css y js

--- index.js
+++ index.js
@@ -34,3 +34,52 @@ window.onload = () => {
         document.querySelector(".header").classList.remove("active");
     }
 }
+ var swiper = new Swiper(".vehicles-slider", {
+    slidesPerView: 1,
+    spaceBetween:20,
+    loop:true,
+    grabCursor:true,
+    autoplay:{
+        delay: 9500,
+        disableOnInteraction:false,
+    },
+    pagination: {
+        el:".swiper-pagination",
+        clickable:true,
+    },
+    breakpoints: {
+        0:{slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {slidesPerView: 3,
+        },
+    },
+ });
+ //featured section inicio//
+
+ var swiper = new Swiper(".featured-slider", {
+    slidesPerView: 1,
+    spaceBetween:20,
+    loop:true,
+    grabCursor:true,
+    autoplay:{
+        delay: 9500,
+        disableOnInteraction:false,
+    },
+    pagination: {
+        el:".swiper-pagination",
+        clickable:true,
+    },
+    breakpoints: {
+        0:{slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        991: {slidesPerView: 3,
+        },
+    },
+ });
+ //featured section fin//
