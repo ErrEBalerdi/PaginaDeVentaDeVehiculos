a4f354274a6c6f53c246b285b71d54017e9199d1
NeyanPorras <72233392+NeyanPorras@users.noreply.github.com>
2023-04-16 00:45:10 -0300

    modal y rating system

--- index.js
+++ index.js
@@ -222,40 +222,91 @@ function showCar(e) {
 /* wizard form */
 
 document.querySelector("#wizard-btn").onclick = () => {
-  document.querySelector(".wizard-form-container").classList.toggle("active")
-}
+  document.querySelector(".wizard-form-container").classList.toggle("active");
+};
 
 document.querySelector("#close-wizard-form").onclick = () => {
-  document.querySelector(".wizard-form-container").classList.remove("active")
-}
+  document.querySelector(".wizard-form-container").classList.remove("active");
+};
 
 /*Stepper del wizard*/
 
-var form1 = document.getElementById("form1"); 
-var form2 = document.getElementById("form2"); 
-var form3 = document.getElementById("form3"); 
+var form1 = document.getElementById("form1");
+var form2 = document.getElementById("form2");
+var form3 = document.getElementById("form3");
 
 var next1 = document.getElementById("next1");
 var next2 = document.getElementById("next2");
 var back1 = document.getElementById("back1");
 var back2 = document.getElementById("back2");
 
-next1.onclick = function(){
-  form1.style.left = "-450px"
-  form2.style.left = "40px"
-} 
-back1.onclick = function(){
-  form1.style.left = "40px"
-  form2.style.left = "450px"
-}
-next2.onclick = function(){
-  form2.style.left = "-450px"
-  form3.style.left = "40px"
-}
-back2.onclick = function(){
-  form2.style.left = "40px"
-  form3.style.left = "450px"
-}
+next1.onclick = function () {
+  form1.style.left = "-450px";
+  form2.style.left = "40px";
+};
+back1.onclick = function () {
+  form1.style.left = "40px";
+  form2.style.left = "450px";
+};
+next2.onclick = function () {
+  form2.style.left = "-450px";
+  form3.style.left = "40px";
+};
+back2.onclick = function () {
+  form2.style.left = "40px";
+  form3.style.left = "450px";
+};
 
 /*Stepper del wizard*/
 /* wizard form */
+
+// rating system
+const stars = document.querySelectorAll(".star");
+const ratingValue = document.querySelector("#value");
+
+let valueRate = 0;
+let rating = {
+  1: "Segui participando",
+  2: "Meh",
+  3: "Algo hicieron bien",
+  4: "Buenardo",
+  5: "Rey de reyes",
+};
+
+stars.forEach(function (star) {
+  star.addEventListener("click", function () {
+    valueRate = parseInt(this.getAttribute("data-value"));
+    setRating();
+  });
+  star.addEventListener("mouseover", function () {
+    resetStars();
+    const currentRating = parseInt(this.getAttribute("data-value"));
+    highlightStars(currentRating);
+  });
+  star.addEventListener("mouseout", function () {
+    resetStars();
+    if (valueRate !== 0) {
+      highlightStars(valueRate);
+    }
+  });
+});
+
+function setRating() {
+  resetStars();
+  for (let i = 0; i < rating; i++) {
+    stars[i].classList.add("selected");
+  }
+  ratingValue.innerHTML = rating[valueRate];
+}
+
+function resetStars() {
+  stars.forEach(function (star) {
+    star.classList.remove("selected");
+  });
+}
+
+function highlightStars(rating) {
+  for (let i = 0; i < rating; i++) {
+    stars[i].classList.add("selected");
+  }
+}
