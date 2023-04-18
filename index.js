e4524c2e032b8ccb489cc4ea46244f02b1dd6c57
NeyanPorras <72233392+NeyanPorras@users.noreply.github.com>
2023-04-18 00:05:54 -0300

    arreglo de botones y validate newsletter email
    falta fixear el validate del newsletter

--- index.js
+++ index.js
@@ -86,11 +86,23 @@ let swiper2 = new Swiper(".featured-slider", {
 //featured section fin riki//
 
 // FormSubmit API - Envio de formularios a gmail a traves de la API FormSubmit
-let submit = document.getElementById("sub-form");
+const formAPI = $("#form-api");
+const submit = $("#sub-form");
+let formAction = formAPI[0].action;
+let inputVal = formAPI[0][0].value;
+formAPI.submit(function (e) {
+  e.preventDefault();
+});
 
-submit.addEventListener("click", (e) => {
-  let inputVal = e.target.form[0].value;
-  e.target.form.action += inputVal;
+submit.click(function () {
+  if (
+    !inputVal.match(
+      /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
+    )
+  ) {
+    formAction += inputVal;
+    formAPI.submit();
+  }
 });
 
 // Catalogo de Vehiculos //
