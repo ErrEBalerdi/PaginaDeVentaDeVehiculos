7f9613862209a05a8f94199d01741e696c6d69a5
NeyanPorras <72233392+NeyanPorras@users.noreply.github.com>
2023-04-17 21:07:03 -0300

    agregado form validator
    cambio html y agregado funcion de validacion para el contact-form

--- index.js
+++ index.js
@@ -201,3 +201,53 @@ const vehiculos = [
     },
   },
 ];
+
+// form validator
+const form = $("#contact-form");
+const nameInput = $("#name");
+const emailInput = $("#email");
+const phoneInput = $("#phone");
+const messageInput = $("#message");
+const submitBtn = $("#btn-submit");
+
+// previene al form de hacer enviar el formulario
+$("#contact-form").submit(function (event) {
+  // prevent the default form submission
+  event.preventDefault();
+});
+
+submitBtn.click(function () {
+  const nameValue = nameInput.val();
+  const emailValue = emailInput.val();
+  const phoneValue = phoneInput.val();
+  const messageValue = messageInput.val();
+
+  if (!nameValue.match(/^[a-zA-ZáéíóúñÑÁÉÍÓÚ]{5,}$/)) {
+    nameInput.css("outline", "1px solid red");
+    nameInput.focus();
+  } else nameInput.css("outline", "1px solid green");
+
+  if (
+    !emailValue.match(
+      /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
+    )
+  ) {
+    emailInput.css("outline", "1px solid red");
+    emailInput.focus();
+  } else emailInput.css("outline", "1px solid green");
+
+  if (phoneValue == "" && !phoneValue.match(/^\d{7,14}$/)) {
+    phoneInput.css("outline", "1px solid red");
+    phoneInput.focus();
+  } else phoneInput.css("outline", "1px solid green");
+
+  if (messageValue.length < 8) {
+    messageInput.css("outline", "1px solid red");
+    messageInput.focus();
+  } else {
+    messageInput.css("outline", "1px solid green");
+  }
+
+  // si todas las validaicones pasan se envia el form
+  form.submit();
+});
